019f01850594f9839e09a4e89dd217dd0ed80a32
“StarWork” <27157812@qq.com>
2020-12-28 17:09:01 +0800

    update P1208,water problems...but remember to output 0 when n==0&&m==0.

---
+++ cmake-build-debug/CMakeFiles/P1208.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1208.dir/P1208.cpp.obj"
+  "P1208.exe"
+  "P1208.exe.manifest"
+  "P1208.pdb"
+  "libP1208.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1208.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
